ed44bcad2106c8562b3b58bf50d398d9d0bde2c0
Usman Adepoju <adepojuusman465@gmail.com>
2024-08-24 18:02:59 +0100

    Persist onboarding state in async storage

--- store/userStore.ts
+++ store/userStore.ts
@@ -1,18 +1,28 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { create } from "zustand";
+import { persist, createJSONStorage } from "zustand/middleware";
 
 type UserStore = {
 	hasFinishedOnboarding: boolean;
 	toggleHadOnboarded: () => void;
 };
 
-export const useUserStore = create<UserStore>((set) => ({
-	hasFinishedOnboarding: false,
-	toggleHadOnboarded: () => {
-		set((state) => {
-			return {
-				...state,
-				hasFinishedOnboarding: !state.hasFinishedOnboarding,
-			};
-		});
-	},
-}));
+export const useUserStore = create(
+	persist<UserStore>(
+		(set) => ({
+			hasFinishedOnboarding: false,
+			toggleHadOnboarded: () => {
+				set((state) => {
+					return {
+						...state,
+						hasFinishedOnboarding: !state.hasFinishedOnboarding,
+					};
+				});
+			},
+		}),
+		{
+			name: "plantly-user-store",
+			storage: createJSONStorage(() => AsyncStorage),
+		}
+	)
+);
