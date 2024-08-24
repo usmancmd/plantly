35e0e9ff15b40f12f739dc531f402f8684e38d84
Usman Adepoju <adepojuusman465@gmail.com>
2024-08-24 18:47:38 +0100

    Add a linear gradient background to onboarding screen

--- app/onboarding.tsx
+++ app/onboarding.tsx
@@ -3,6 +3,8 @@ import { theme } from "@/theme";
 import { useUserStore } from "@/store/userStore";
 import { useRouter } from "expo-router";
 import { PlantlyButton } from "@/components/PlantlyButton";
+import { LinearGradient } from "expo-linear-gradient";
+import { StatusBar } from "expo-status-bar";
 
 export default function OnboardingScreen() {
 	const router = useRouter();
@@ -12,9 +14,14 @@ export default function OnboardingScreen() {
 		router.replace("/");
 	};
 	return (
-		<View style={styles.container}>
+		<LinearGradient
+			start={{ x: 0, y: 0 }}
+			end={{ x: 1, y: 1 }}
+			colors={[theme.colorGreen, theme.colorAppleGreen, theme.colorLimeGreen]}
+			style={styles.container}>
+			<StatusBar style="light" />
 			<PlantlyButton title="Let me in!" onPress={handlePress} />
-		</View>
+		</LinearGradient>
 	);
 }
 
