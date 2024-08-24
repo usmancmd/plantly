2f468c5e7ab800ec448b88e087796ba057aa1b11
Usman Adepoju <adepojuusman465@gmail.com>
2024-08-24 16:07:12 +0100

    Add layout group and onboarding screen

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,36 @@
+import { Redirect, Tabs } from "expo-router";
+import Entypo from "@expo/vector-icons/Entypo";
+import Feather from "@expo/vector-icons/Feather";
+import { theme } from "@/theme";
+
+const hasfinishedOnbarding = true;
+
+export default function Layout() {
+	if (!hasfinishedOnbarding) {
+		return <Redirect href="/onboarding" />;
+	}
+	return (
+		<Tabs screenOptions={{ tabBarActiveTintColor: theme.colorGreen }}>
+			<Tabs.Screen
+				name="index"
+				options={{
+					title: "Home",
+					tabBarShowLabel: false,
+					tabBarIcon: ({ size, color }) => (
+						<Entypo name="leaf" size={size} color={color} />
+					),
+				}}
+			/>
+			<Tabs.Screen
+				name="profile"
+				options={{
+					title: "Profile",
+					tabBarShowLabel: false,
+					tabBarIcon: ({ size, color }) => (
+						<Feather name="user" size={size} color={color} />
+					),
+				}}
+			/>
+		</Tabs>
+	);
+}
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,31 +1,10 @@
-import { Tabs } from "expo-router";
-import Entypo from "@expo/vector-icons/Entypo";
-import Feather from "@expo/vector-icons/Feather";
-import { theme } from "@/theme";
+import { Stack } from "expo-router";
 
-export default function Layout() {
+export default function RootLayout() {
 	return (
-		<Tabs screenOptions={{ tabBarActiveTintColor: theme.colorGreen }}>
-			<Tabs.Screen
-				name="index"
-				options={{
-					title: "Home",
-					tabBarShowLabel: false,
-					tabBarIcon: ({ size, color }) => (
-						<Entypo name="leaf" size={size} color={color} />
-					),
-				}}
-			/>
-			<Tabs.Screen
-				name="profile"
-				options={{
-					title: "Profile",
-					tabBarShowLabel: false,
-					tabBarIcon: ({ size, color }) => (
-						<Feather name="user" size={size} color={color} />
-					),
-				}}
-			/>
-		</Tabs>
+		<Stack>
+			<Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+			<Stack.Screen name="onboarding" />
+		</Stack>
 	);
 }
